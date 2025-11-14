f39f9683b47a988b710cfd76d99c5a2491dd2ccf
Daniel Hannemann <123128332+dhannemann@users.noreply.github.com>
2025-11-13 22:41:23 -0300

    Update config.php

--- acamados/config.php
+++ acamados/config.php
@@ -26,14 +26,5 @@ if ($conn->connect_error) {
 
 // Boa prática: Definir o charset para evitar problemas com acentos
 $conn->set_charset("utf8mb4");
-?>
 
-<?php
-//$conn = new mysqli('localhost', 'root', '', 'acamados', 3307);
-//$conn = new MySQLi('localhost', 'biometria', 'Biometria@T12s14r2', 'biometria');
-//date_default_timezone_set('America/Sao_Paulo');
-
-//if ($conn->connect_error) {
-    //die("Erro na conexão com o banco de dados: " . $conn->connect_error);
-//}
 ?>
